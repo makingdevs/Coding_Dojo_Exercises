0c28863da115b270eec2a7b6905b4f4217d83cb3
temocpalma <temopalmag@gmail.com>
2016-03-08 20:11:56 -0600

    Se ajustan las pruebas considerando las nuevas condiciones del tablero

--- peg_game/spec/peg_game_spec.rb
+++ peg_game/spec/peg_game_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe PegGame do
   it "should create a new board of 5x9" do
     expect(game.board.size).to eq(5)
     expect(game.row_at(0)).to match_array(['X','.','X','.','X','.','X','.','X'])
-    expect(game.row_at(1)).to match_array(['X','.','X','.','X','.','X'])
+    expect(game.row_at(1)).to match_array(['_','X','.','X','.','X','.','X','_'])
   end
 
   [
@@ -18,11 +18,11 @@ RSpec.describe PegGame do
       game_row = game.board[row]
       spikes = game_row.count('X')
       expect(spikes).to eq size
-      game.quitSpikeIn(row, col)
-
-      game_row = game.board[row]
-      spikes = game_row.count('X')
-      expect(spikes).to eq(size - 1)
+      if game.quitSpikeIn(row, col) then
+        game_row = game.board[row]
+        spikes = game_row.count('X')
+        expect(spikes).to eq(size - 1)
+      end
     end
   end
 
