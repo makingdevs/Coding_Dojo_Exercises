92c2438f225ecae40f477657f3aa0f2e77fb578d
temocpalma <temopalmag@gmail.com>
2016-03-08 20:11:00 -0600

    Se ajusta la construcción del tablero inicial de forma cuadrada, se valida si existe un spike en la posición indicada para quitarlo, se elimina el join con puntos de la función row_at

--- peg_game/lib/peg_game.rb
+++ peg_game/lib/peg_game.rb
@@ -5,19 +5,41 @@ class PegGame
     @board = []
 
     rows.times do | row |
+      r = []
       if(row % 2 == 0) then
-        @board << ['X','X','X','X','X']
+        cols.times do | col |
+          if (col % 2 == 0) then
+            r << 'X'
+          else
+            r << '.'
+          end
+        end
+        @board << r
       else
-        @board << ['X','X','X','X']
+        cols.times do | col |
+          if (col == 0 or col == cols-1) then
+            r << '_'
+          elsif (col % 2 == 0) then
+            r << '.'
+          else
+            r << 'X'
+          end
+        end
+        @board << r
       end
     end
   end
 
   def quitSpikeIn(row,col)
-    @board[row][col] = "."
+    if @board[row][col] == "X" then
+      @board[row][col] = "."
+      return true
+    else
+      return false
+    end
   end
 
   def row_at(row)
-    @board[row].join('.').chars
+    @board[row]
   end
 end
