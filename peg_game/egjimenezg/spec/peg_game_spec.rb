aaeb2256d25ce461485f92a681336084c5ed2976
Gamaliel Jiménez <egjimenezg@Gamaliels-MBP.lan>
2016-03-16 22:54:04 -0600

    Updating tests

--- peg_game/egjimenezg/spec/peg_game_spec.rb
+++ peg_game/egjimenezg/spec/peg_game_spec.rb
@@ -6,33 +6,27 @@ RSpec.describe PegGame do
 
   it "should create a new board of RxC" do
     expect(game.board.size).to eq(5) 
+    expect(game.board[0]).to match_array(['X','.','X','.','X','.','X','.','X'])
+    expect(game.board[1]).to match_array([' ','X','.','X','.','X','.','X',' '])
   end 
 
-  [
-    [1, 1, 4],
-    [2, 1, 5],
-    [3, 2, 4]
-  ].each do |row, col, size|
-    it "should quit spike in (#{row}, #{col}) from board" do
-      game_row = game.board[row]
-      p game_row
-      spikes = game_row.count('X')
-      expect(spikes).to eq size
-      game.quitSpikeIn(row, col)
+  #[
+  #  [1, 1, 4],
+  #  [2, 1, 5],
+  #  [3, 2, 4]
+  #].each do |row, col, size|
+  #  it "should quit spike in (#{row}, #{col}) from board" do
+  #    game_row = game.board[row]
+  #    p game_row
+  #    spikes = game_row.count('X')
+  #    expect(spikes).to eq size
+  #    game.quitSpikeIn(row, col)
 
-      game_row = game.board[row]
-      spikes = game_row.count('X')
-      expect(spikes).to eq(size - 1)
-    end
-  end
+  #    game_row = game.board[row]
+  #    spikes = game_row.count('X')
+  #    expect(spikes).to eq(size - 1)
+  #  end
+  #end
   
-  it "should calculate the probability from the column of the first row until the target column in the last row" do
-    missingPegs = [[1,1],[2,1],[3,2]]
-    missingPegs.each do |x, y|
-      game.quitSpikeIn(x,y) 
-    end 
-   
-    expect(game.getProbabilityFromColumnToTargetColumn(0,0)).to eq(0.5)
-  end
-
+  
 end 
