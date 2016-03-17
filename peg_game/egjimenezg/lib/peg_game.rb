f8e3a433d70303b6c2faaa970fa645ce9759eddd
egjimenezg <egjimenezg@gmail.com>
2016-03-16 22:55:41 -0600

    Creating board

--- peg_game/egjimenezg/lib/peg_game.rb
+++ peg_game/egjimenezg/lib/peg_game.rb
@@ -2,15 +2,32 @@ class PegGame
   attr_accessor :board
 
   def initialize(rows,cols)
-    @board << ('X'*((row % 2 == 0) ? cols : (cols-1))).split
+    @board = []
+    rows.times do | row |
+      @board << []    
+
+      colsForRow = cols-1
+      displacement = 0
+      
+      if(row % 2 != 0) then
+        @board[row][0] = " "
+        @board[row][(cols*2)-2] = " "
+        colsForRow -= 1
+        displacement = 1
+      end
+
+      colsForRow.times do | col |
+        @board[row][(col*2)+displacement] = "X"
+        @board[row][(col*2)+1+displacement] = "."
+      end
+
+      @board[row][(colsForRow*2)+displacement] = "X"
+
+    end
   end
 
-  def quitSpikeIn(row,col)
-    @board[row][col] = "."
-  end
-
-  def getProbabilityFromColumnToTargetColumn(originColumn,targetColumn){
-
-  }
+ # def quitSpikeIn(row,col)
+ #   @board[row][col] = "."
+ # end
 
 end
