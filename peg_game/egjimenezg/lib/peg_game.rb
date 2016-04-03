e544884e8daf783f81c51e9f5a9cef99ea105882
egjimenezg <egjimenezg@gmail.com>
2016-04-03 01:33:33 -0600

    Adding method that returns the probability of the fall from one column to the target column

--- peg_game/egjimenezg/lib/peg_game.rb
+++ peg_game/egjimenezg/lib/peg_game.rb
@@ -1,8 +1,11 @@
+require 'bigdecimal'
+
 class PegGame
-  attr_accessor :board,:cols
+  attr_accessor :board,:probabilityMatrix,:cols
 
   def initialize(rows,cols)
     @board = []
+    @probabilityMatrix = []
     @cols = cols
 
     rows.times do | row |
@@ -24,8 +27,16 @@ class PegGame
       end
 
       @board[row][(colsForRow*2)+displacement] = "X"
-
     end
+   
+    rows.times do | row |
+      probabilityMatrix << [] 
+
+      (((cols-1)*2)-1).times do | col |
+        probabilityMatrix[row][col] = 0
+      end
+    end 
+
   end
 
   def quitSpikeIn(row,col)
@@ -38,16 +49,16 @@ class PegGame
     columns.each do | column |
       if(@board[row+1][column+1] == 'X') then
         if(column == 0) then
-          columnProbabilities << {:column => column+1,:parent => column,:probability => 1.0}
+          columnProbabilities << {:column => column+1,:parent => column,:probability => BigDecimal.new("1.0")}
         elsif(column == ((@cols-2)*2)) then
-          columnProbabilities << {:column => column-1,:parent => column,:probability => 1.0} 
+          columnProbabilities << {:column => column-1,:parent => column,:probability => BigDecimal.new("1.0")} 
         else
           if(column - targetColumn >= 0) then
-            columnProbabilities << {:column => column-1,:parent => column,:probability=> 0.5}
+            columnProbabilities << {:column => column-1,:parent => column,:probability=> BigDecimal.new("0.5")}
           end
 
           if(column+1-targetColumn+(row+1) < @board.size) then
-            columnProbabilities << {:column => column+1,:parent => column,:probability=> 0.5} 
+            columnProbabilities << {:column => column+1,:parent => column,:probability=> BigDecimal.new("0.5")} 
           end
         end
       end
@@ -55,6 +66,27 @@ class PegGame
     end
 
     columnProbabilities
+  end
+
+  def getProbabilityFromColumnToTargetColumn(originColumn,targetColumn)
+    targetColumn *= 2
+    probabilityMatrix[0][originColumn*2] = BigDecimal.new("1.0")
+    columns = [originColumn*2]
+    probabilityRows = []
+
+    (@board.size-1).times do | row | 
+      probabilityRows = getProbabilityForPositions(row,columns.uniq,targetColumn)  
+
+      probabilityRows.each do | probabilityRow |
+        probabilityMatrix[row+1][probabilityRow[:column]] += (probabilityMatrix[row][probabilityRow[:parent]]*probabilityRow[:probability])
+        probabilityMatrix[row+1][probabilityRow[:column]] = probabilityMatrix[row+1][probabilityRow[:column]].truncate(4)
+      end
+      
+      columns.clear 
+      columns = probabilityRows.collect{ | probabilityRow | probabilityRow[:column] }
+    end
+     
+    probabilityMatrix[@board.size-1][targetColumn*2]
   end
 
   def isOdd(row)
