60aac760f5649c3c59034058dfd76ca857094d18
egjimenezg <egjimenezg@gmail.com>
2016-04-03 01:09:47 -0600

    Adding new test to calculate the probability from one column to the target column

--- peg_game/egjimenezg/spec/peg_game_spec.rb
+++ peg_game/egjimenezg/spec/peg_game_spec.rb
@@ -28,14 +28,18 @@ RSpec.describe PegGame do
   end
 
   [
-    [0,[0],0,[{:column => 1,:parent => 0,:probability => 1.0}]],
-    [0,[6],0,[{:column => 5,:parent => 6,:probability => 1.0}]],
-    [0,[4],0,[{:column => 3,:parent => 4,:probability => 0.5}]]
+    [0,[0],0,[{:column => 1,:parent => 0,:probability => BigDecimal.new("1.0")}]],
+    [0,[6],0,[{:column => 5,:parent => 6,:probability => BigDecimal.new("1.0")}]],
+    [0,[4],0,[{:column => 3,:parent => 4,:probability => BigDecimal.new("0.5")}]]
   ].each do | row, columns, targetColumn, columnsWithProbability |
     it "should calculate the probabilities of the ways when the ball drop from columns #{columns}" do
       probabilities = game.getProbabilityForPositions(row,columns,targetColumn)
       expect(probabilities).to match_array(columnsWithProbability)
     end 
   end
-  
+ 
+  it "should calculate the probability of fall from the first column to the target column" do
+    probability = game.getProbabilityFromColumnToTargetColumn(0,0)
+    expect(probability).to eq(0.5)
+  end
 end 
